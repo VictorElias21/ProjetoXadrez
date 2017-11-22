4983467dd2c9324298e5961f94e261e5b0a1dbf0
VictorElias21 <itz.victor.elias@gmail.com>
2017-11-22 11:31:47 -0200

    Projeto iniciado

---
+++ ProjetoXadrez/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using tabuleiro;
+
+namespace ProjetoXadrez
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+        }
+    }
+}
